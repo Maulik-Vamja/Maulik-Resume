edc6b8d436930e1ddf396f0277d157a76f185eba
Maulik-Vamja <maulikvamja1@gmail.com>
2021-12-17 21:01:12 +0530

    All Work Done

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+		<link rel="stylesheet" type="text/css" href="style.css" media="screen and (min-width: 769px)"/>
+		<title>Maulik Vamja-Resume</title>
+	</head>
+	<body>
+		<div class="container">
+			<!--left column starts-->
+			<div class="left_col">
+				<div class="left_col_1">
+					<img src="./img1.jpg" alt="profile_picture" height="190" />
+					<p class="name">Maulik D. Vamja</p>
+					<p class="profession">Back End Developer</p>
+				</div>
+				
+				<div class="left_col_2">
+					<p class="header header2">Contact</p>
+					<p class="address">
+						From Surat, India<br /><br />
+						<a href="mailto:maulikvamja1@gmail.com" style="text-decoration:none;color:#3D3D3D;">maulikvamja1@gmail.com</a><br /><br />
+						+918905802912
+					</p>
+				</div>
+
+				<div class="left_col_3">
+					<div class="skill">
+						<p class="header header3">Skills</p>
+					</div>
+					<ul>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 88%;">
+									<p class="align">CORE PHP</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 80%;">
+									<p class="align">LARAVEL</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 60%;">
+									<p class="align">HTML5</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 60%;">
+									<p class="align">CSS3</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 60%;">
+									<p class="align">BOOTSTRAP</p>
+								</span>
+							</div>
+						</li>
+						
+						<li>
+							<div class="progress_bar">
+								<span style="width: 55%;">
+									<p class="align">UI/UX</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 60%;">
+									<p class="align">GIT & GITHUB</p>
+								</span>
+							</div>
+						</li>
+						<li>
+							<div class="progress_bar">
+								<span style="width: 80%;">
+									<p class="align">WORDPRESS</p>
+								</span>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<!--right column starts-->
+			<div class="right_col">
+				<div class="right_col_1">
+					<p class="header header4">PROFILE</p>
+					<p class="profile profile1">
+						I'm Maulik Vamja. I am a Back end web developer. I have experience
+						in core php, laravel framework, html5, css3, wordpress web developement. I
+						have 2+ years of experience in back end web development. I have
+						build 50+ freelance projects also. I am very passionate about back
+						end development.
+					</p>
+				</div>
+				<div class="right_col_2">
+					<p class="header header5">EDUCATION</p>
+					<div class="right_col_left1">
+						<p class="header header6">Post Gratuation</p>
+						<p class="header below">2020 - PRESENT</p>
+						<p class="header header6" style="top: 110px;">Graduation</p>
+						<p class="header below" style="top: 130px;">2017 - 20</p>
+						<p class="header header6" style="top: 202px;">HSC</p>
+						<p class="header below" style="top: 220px;">2016 - 17</p>
+					</div>
+					<div class="right_col_right1">
+						<p class="side">
+							Parul University
+							<br />
+							Master in computer application
+							<br />
+							Vadodara, Gujarat.
+						</p>
+						<p class="side" style="top: 106px;">
+							VNSGU
+							<br />
+							Bachelor of Computer Applications
+							<br />
+							Surat, Gujarat
+						</p>
+						<p class="side" style="top: 198px;">
+							Saraswati Vidhyalay
+							<br />
+							Commerce
+							<br />
+							Surat, Gujarat
+						</p>
+					</div>
+				</div>
+
+				<div class="right_col_3">
+					<p class="header header5">Experience</p>
+					<div class="right_col_left1">
+						<p class="header header6">PHP Developer</p>
+						<p class="header below">2021-Present</p>
+						<p class="header header6" style="top: 110px;">Web Developer</p>
+						<p class="header below" style="top: 130px;">2018 - 19</p>
+					</div>
+					<div class="right_col_right1">
+						<p class="side">
+							Yudiz Solution Pvt. Ltd.
+							<br />
+							As a Trainee PHP Developer
+							<br />
+							Ahmedabad, Gujarat.
+						</p>
+						<p class="side" style="top: 106px;">
+							VNSGU
+							<br />
+							As a Junior Web Developer
+							<br />
+							Surat, Gujarat
+						</p>
+					</div>
+				</div>
+
+				<div class="right_col_4">
+					<p class="header header5">Interested Area</p>
+					<div class="recent">
+						<p class="side">
+							Quantum Computing, PHP, Javascript(Nodejs), Wordpress
+						</p>
+					</div>
+				</div>
+
+				<div class="right_col_5">
+					<p class="header header5">Declaration</p>
+					<div class="recent">
+						<p class="side">
+							I here by that the details furnished above the true and correct<br />
+							to the best of my knowledge and belief.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
